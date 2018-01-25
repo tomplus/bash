451ad54e916212f5371f92f75effddcb9391778f
Tomasz Prus <tomasz.prus@gmail.com>
2018-01-26 00:52:28 +0100

    feat: add bashrc

---
+++ src/bashrc-tp.sh
@@ -0,0 +1,36 @@
+if [ "$PS1" ]; then
+
+    # Neverending history
+    export HISTFILESIZE=9999999999
+    export HISTSIZE=9999999999
+
+    # Ignore simple commands
+    export HISTIGNORE="ls:ls -alh:ll:ll -h:history:top:ps:ps axf:h:hh:hhh:hs"
+
+    # Save after every command
+    shopt -s histappend
+    export PROMPT_COMMAND="history -a;"
+
+    # h - history, hh - top 20 from last 1000, hhh - top 20 ever, hs - top ssh
+    alias h="history"
+
+    # hh - last top 20
+    alias hh="tail -n 20000 ~/.bash_history | sort | uniq -c | sort -nr | head -n 20 | awk '{ gsub(/^ *[0-9]+ +/, \"\"); print;}'"
+
+    # hhh - top 20 ever
+    alias hhh="sort ~/.bash_history | sort | uniq -c | sort -nr | head -n 20 | awk '{ gsub(/^ *[0-9]+ +/, \"\"); print;}'"
+
+    # hs - top ssh/scp
+    alias hs="egrep \"^(ssh|scp)\" ~/.bash_history | sort | uniq -c | sort -nr | head -n 20 | awk '{ gsub(/^ *[0-9]+ +/, \"\"); print;}'"
+
+
+    # Kubernetes
+    if type kubectl >/dev/null 2>&1; then
+
+        source <(kubectl completion bash)
+        alias k="kubectl"
+        alias ka='kubectl --all-namespaces'
+
+    fi
+
+fi
